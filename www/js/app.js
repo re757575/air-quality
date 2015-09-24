a8a9c7ba7a5150dc1836d1d4d2700f9204e5a33f
Alex.Dai <re757575@gmail.com>
2015-09-24 14:58:10 +0800

    Modify: 重構, 獨立 view 與 controllers

--- www/js/app.js
+++ www/js/app.js
@@ -3,7 +3,7 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-var app = angular.module('starter', ['ionic']);
+var app = angular.module('starter', ['ionic', 'starter.controllers']);
 
 app.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
@@ -36,11 +36,11 @@ app.config(function ($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('/home');
   $stateProvider.state('home', {
       url: '/home',
-      templateUrl: 'home.html'
+      templateUrl: 'templates/home.html'
   })
   .state('page1', {
       url: '/page1',
-      templateUrl: 'page1.html'
+      templateUrl: 'templates/page1.html'
   });
 });
 
---
+++ www/js/controllers.js
@@ -0,0 +1,4 @@
+// Ionic Starter Controllers
+angular.module('starter.controllers', []);
+
+console.log('starter.controllers');
