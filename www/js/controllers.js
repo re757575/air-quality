7fe46d3e157bd21fe9e5caba335863232b7e030c
Alex.Dai <re757575@gmail.com>
2015-09-25 13:56:36 +0800

    Added: pull to refresh

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -96,6 +96,74 @@ app.controller('airCtrl', function($scope, $stateParams, $http, $ionicLoading) {
         alert(status);
 
     });
+
+
+  $scope.doRefresh = function() {
+
+    var param = {
+        '$filter': "County eq '"+ $scope.city.q +"'",
+        '$orderby': 'SiteName',
+        '$skip': 0,
+        '$top': 1000,
+        'format': 'json',
+        'callback': 'JSON_CALLBACK'
+    };
+
+    var paramStr = Object.keys(param).map(function(key) {
+        return key + '=' + param[key];
+    }).join('&');
+
+    var url = encodeURI("http://opendata.epa.gov.tw/ws/Data/AQX/?"+ paramStr);
+
+    $http.jsonp(url)
+    .success(function (data, status, headers, config, statusText) {
+        console.log(data);
+        console.log(status);
+
+        var imageBaseUrl = 'img/face-icon/48/';
+        for (var i in data) {
+
+            var PSI = data[i].PSI;
+            var PM25 = data[i]['PM2.5'];
+
+            if (PM25 === '') {
+                data[i]['PM2.5'] = '通訊異常 或 設備維護';
+            }
+
+            if (PSI <= 50) {
+                // 良好
+                data[i]['img'] = imageBaseUrl + 'laughing-face.png';
+            } else if (PSI >= 51 && PSI <= 100) {
+                // 普通
+                data[i]['img'] = imageBaseUrl + 'neutral-face.png';
+            } else if (PSI >= 101 && PSI <= 199) {
+                // 不良
+                data[i]['img'] = imageBaseUrl + 'sad-face-eyebrows.png';
+            } else if (PSI >= 200 && PSI <= 299) {
+                // 非常不良
+                data[i]['img'] = imageBaseUrl + 'angry-face.png';
+            } else {
+                // 有害
+                data[i]['img'] = imageBaseUrl + 'angry-face-teeth.png';
+            }
+        }
+
+        $scope.airList = data;
+        $ionicLoading.hide();
+    })
+    .error(function(data, status) {
+        console.log(data);
+        console.log(status);
+        $ionicLoading.hide();
+        alert(status);
+
+    }).finally(function() {
+       // Stop the ion-refresher from spinning
+       $scope.$broadcast('scroll.refreshComplete');
+    });
+
+  };
+
 });
 
 var citys = [
