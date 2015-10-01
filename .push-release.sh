c1b34fda83ccdd73295280317d41d4d0fd8325ea
Alex.Dai <re757575@gmail.com>
2015-10-01 18:45:25 +0800

    Test: deploy

---
+++ .push-release.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git config --global user.name "Alex.Dai (via Travis-CI)"
+git config --global user.email "re757575@gmail.com"
+git remote set-url origin https://${GH_TOKEN}@github.com/re757575/my-ionic.git
+git fetch --tags
+
+export VERSION=`grep -Po '(?<="version": ")[^"]*' package.json`
+
+if [ -z "${TRAVIS_TAG}" ]; then git tag $VERSION; fi
+if [ -z "${TRAVIS_TAG}" ]; then git push --tags --quiet; fi
+
+if [ -z "${TRAVIS_TAG}" ]; then cd www; git init; git remote add origin https://${GH_TOKEN}@github.com/re757575/my-ionic.git; git add .; git commit -a -m "init"; git push -u --quiet origin -f master:gh-pages; cd ../; fi
