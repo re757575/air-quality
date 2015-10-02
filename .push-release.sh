baa7fbf875c6de51daaea56e1da099e69b2326ab
Alex.Dai <re757575@gmail.com>
2015-10-02 14:38:04 +0800

    Added: sign APK

--- .push-release.sh
+++ .push-release.sh
@@ -10,4 +10,4 @@ export VERSION=`grep -Po '(?<="version": ")[^"]*' package.json`
 if [ -z "${TRAVIS_TAG}" ]; then git tag $VERSION; fi
 if [ -z "${TRAVIS_TAG}" ]; then git push --tags --quiet; fi
 
-if [ -z "${TRAVIS_TAG}" ]; then cd www; git init; git remote add origin https://${GH_TOKEN}@github.com/re757575/my-ionic.git; git add .; git commit -a -m "init"; git push -u --quiet origin -f master:gh-pages; cd ../; fi
+if [ -z "${TRAVIS_TAG}" ]; then cd www; git init; git remote add origin https://${GH_TOKEN}@github.com/re757575/my-ionic.git; git add .; git commit -a -m "init [ci skip]"; git push -u --quiet origin -f master:gh-pages; cd ../; fi
