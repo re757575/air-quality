fe56c0c300fac170129682ff306dc8257bee8d99
Alex.Dai <re757575@gmail.com>
2015-10-27 15:04:35 +0800

    Modify: .travis.yml

--- .push-release.sh
+++ .push-release.sh
@@ -5,9 +5,10 @@ git config --global user.email "re757575@gmail.com"
 git remote set-url origin https://${GH_TOKEN}@github.com/re757575/air-quality.git
 git fetch --tags
 
+export TIME_STR=`eval date`
 export VERSION=`grep -Po '(?<="version": ")[^"]*' package.json`
 
 if [ -z "${TRAVIS_TAG}" ]; then git tag $VERSION; fi
 if [ -z "${TRAVIS_TAG}" ]; then git push --tags --quiet; fi
 
-if [ -z "${TRAVIS_TAG}" ]; then cd www; git init; git remote add origin https://${GH_TOKEN}@github.com/re757575/air-quality.git; git add .; git commit -a -m "init [ci skip]"; git push -u --quiet origin -f master:gh-pages; cd ../; fi
+if [ -z "${TRAVIS_TAG}" ]; then cd www; git init; git remote add origin https://${GH_TOKEN}@github.com/re757575/air-quality.git; git add .; git commit -a -m "Travis CI build on $TIME_STR [ci skip]"; git push -u --quiet origin -f master:gh-pages; cd ../; fi
